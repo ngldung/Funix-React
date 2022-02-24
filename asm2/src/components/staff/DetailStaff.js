a0918e1f742df45cc3251834fe70e9d614339443
Nguyen Luong Dung <luongdung.tdc@gmail.com>
2022-02-24 09:20:42 +0700

    requirement 2 - add DetailStaff pages

--- asm2/src/components/staff/DetailStaff.js
+++ asm2/src/components/staff/DetailStaff.js
@@ -1,5 +1,28 @@
+import classes from "./DetailStaff.module.css";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+
 const DetailStaff = (props) => {
-  return <div></div>;
+  return (
+    <div>
+      <Breadcrumb>
+        <Breadcrumb.Item href="/">Nhân Viên</Breadcrumb.Item>
+        <Breadcrumb.Item active>{props.name}</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="row">
+        <div className="col-12 col-md-4 col-lg-3">
+          <img className={classes.img} src={props.image} alt={props.name} />
+        </div>
+        <div className="col-12 col-md-8 col-lg-9">
+          <h3>Họ và tên: {props.name}</h3>
+          <p>Ngày sinh: {props.doB}</p>
+          <p>Ngày vào công ty: {props.startDate}</p>
+          <p>Phòng ban: {props.department}</p>
+          <p>Số ngày nghỉ còn lại: {props.annualLeave}</p>
+          <p>Số ngày đã làm thêm: {props.overTime}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailStaff;
--- asm2/src/pages/StaffDetail.js
+++ asm2/src/pages/StaffDetail.js
@@ -1,5 +1,34 @@
+import { Fragment } from "react";
+import { useParams } from "react-router-dom";
+import DetailStaff from "../components/staff/DetailStaff";
+import { STAFFS } from "../shared/staffs";
+
 const StaffDetail = () => {
-  return <div></div>;
+  const params = useParams();
+  console.log(params.staffId);
+  const staffsList = STAFFS;
+  const selectedStaff = staffsList.find((staff) => staff.id.toString() === params.staffId);
+  console.log(selectedStaff);
+  if (!selectedStaff) {
+    return <p>NO STAFF FOUND</p>;
+  }
+  const isoStartDate = new Date(selectedStaff.startDate);
+  const startDate = isoStartDate.toLocaleDateString('en-GB');
+  const isoDOB = new Date(selectedStaff.doB);
+  const doB = isoDOB.toLocaleDateString('en-GB');
+  return (
+    <Fragment>
+      <DetailStaff
+        image={selectedStaff.image}
+        name={selectedStaff.name}
+        doB={doB}
+        startDate={startDate}
+        department={selectedStaff.department.name}
+        annualLeave={selectedStaff.annualLeave}
+        overTime={selectedStaff.overTime}
+      />
+    </Fragment>
+  );
 };
 
 export default StaffDetail;
