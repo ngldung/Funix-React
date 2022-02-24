acc8b5f87d917d45b5be49526f944c142ec947ad
Nguyen Luong Dung <luongdung.tdc@gmail.com>
2022-02-24 09:24:23 +0700

    requirement 3 - build Department page

--- asm2/src/pages/Department.js
+++ asm2/src/pages/Department.js
@@ -1,4 +1,15 @@
+import { DEPARTMENTS } from "../shared/staffs";
+
 const Department = () => {
-  return <div></div>;
+  return (
+    <div className="row">
+      {DEPARTMENTS.map((dept) => (
+        <div key={dept.id} className="col-12 col-md-6 col-lg-4">
+          <h1>{dept.name}</h1>
+          <p>Số lượng nhân viên: {dept.numberOfStaff}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 export default Department;
