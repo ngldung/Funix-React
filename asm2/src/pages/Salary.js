4097e60a9dc7e43488f41e5942ee702b225b475a
Nguyen Luong Dung <luongdung.tdc@gmail.com>
2022-02-24 09:25:58 +0700

    requirement 4 - build Salary page

--- asm2/src/pages/Salary.js
+++ asm2/src/pages/Salary.js
@@ -1,4 +1,35 @@
+import { STAFFS } from "../shared/staffs";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+import Card from "../components/UI/Card";
+
 const Salary = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Breadcrumb>
+        <Breadcrumb.Item href="/">Nhân Viên</Breadcrumb.Item>
+        <Breadcrumb.Item active>Bảng Lương</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="row">
+        {STAFFS.map((staff) => (
+          <div key={staff.id} className="col-12 col-md-6 col-lg-4">
+            <Card>
+              <h3>{staff.name}</h3>
+              <p>Mã nhân viên: {staff.id}</p>
+              <p>Hệ số lương: {staff.salaryScale}</p>
+              <p>Số giờ làm thêm: {staff.overTime}</p>
+              <hr />
+              <p>
+                Lương:{" "}
+                {(
+                  staff.salaryScale * 3000000 +
+                  (staff.overTime / 8) * 200000
+                ).toFixed(0)}
+              </p>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Salary;
