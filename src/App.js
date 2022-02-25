8db8e223523b0e76f69e4889e72da3b932afd049
Nguyen Luong Dung <luongdung.tdc@gmail.com>
2022-02-25 12:05:01 +0700

    Component Part 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
